cc87035cc395c70fa68ea6a2299be4df274e4d85
alahab <maria@mariaswriting.com>
2015-05-18 21:47:38 +0800

    Layout is working

---
+++ PhotoPortfolio/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  PhotoPortfolio
+//
+//  Created by Mariya Pavlyuchenko on 18/05/15.
+//  Copyright (c) 2015 Mariya Pavlyuchenko. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imgCell: UIImageView!
+    
+    @IBOutlet var lblCell: UILabel!
+    
+    
+    
+    
+}
--- PhotoPortfolio/ViewController.swift
+++ PhotoPortfolio/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+    
+    var tableImages = ["ice_in_nowray.jpg", "RWANDA_terraced_field_agriculture.jpg", "NEW_ZEALAND_Tui_Bird_Kowhai_Tree.jpg", "ice_in_nowray.jpg", "RWANDA_terraced_field_agriculture.jpg", "NEW_ZEALAND_Tui_Bird_Kowhai_Tree.jpg", "ice_in_nowray.jpg", "RWANDA_terraced_field_agriculture.jpg", "NEW_ZEALAND_Tui_Bird_Kowhai_Tree.jpg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tableImages.count
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CollectionViewCell
+        
+        cell.lblCell.text = tableImages[indexPath.row]
+        cell.imgCell.image =  UIImage(named: tableImages[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        println("image \(indexPath.row) is selected")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+            
+          var width = view.frame.width/2.35
+            
+          return CGSize(width: width, height: width)
+    }
 
 }
 
